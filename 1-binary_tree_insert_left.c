52f1841528a90d6db117d25c4fc67ebba468c1ae
Elias Martin <5715@holbertonstudents.com>
2023-01-11 07:37:35 -0800

    insert_left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,6 +11,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 binary_tree_t *newnode;
 struct binary_tree_s *temporal;
 
+if(!parent)
+{
+	return (NULL);
+}
+
 newnode = malloc(sizeof(binary_tree_t));/**memory allocate for new node*/
 if (newnode == NULL)/** verification of suceffuly*/
 {
@@ -22,7 +27,7 @@ if (!parent->left)
 	newnode->parent = parent;
 	parent->left = newnode;
 	}
-else
+else if (parent->left)
 	{
 	temporal = parent->left;
 	newnode->parent = parent;
