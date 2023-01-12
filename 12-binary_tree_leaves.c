bb0fc1d2364671ad87458f5f8b422683abf19828
Maurlcio <thecoolermess@gmail.com>
2023-01-12 07:23:22 -0800

    added a function that counts the leaves in a tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,29 +1,25 @@
 #include "binary_trees.h"
+
 /**
-  * binary_tree_leaves - cuantas hojas.
-  * @tree: first node.
-  * Return: total leaves.
-  */
+ * binary_tree_leaves - counts the amount of leaves in a binary tree
+ * with recursion of course, because this is like the only time
+ * ever since we learnt that that I feel compelled to use it
+ * and you know damn well I'm gonna milk that fact
+ *
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: amount of leaves, or 0 if tree does not exist
+ */
+
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-size_t l = 0, la = 0;
+	size_t treeleaves = 0;
 
-if (!tree->left && !tree->right)
-{
-	l += 1;
-}
-if (tree->left || tree->right)
-{
-if (tree->left)
-{
-binary_tree_leaves(tree->left);
-l += 1;
-}
-if (tree->right)
-{
-binary_tree_leaves(tree->right);
-la += 1;
-}
-}
-return (l + la);
+	if (!tree)
+		return (0);
+
+	treeleaves += (!tree->left && !tree->right) ? 1 : 0;
+	treeleaves += binary_tree_leaves(tree->left);
+	treeleaves += binary_tree_leaves(tree->right);
+	return (treeleaves);
 }
